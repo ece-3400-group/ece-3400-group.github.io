95cb6fb7834da5a3b12c9674cef38814f1f43d7d
youngpines <hy379@cornell.edu>
2018-09-06 21:59:20 -0400

    added comments to code and changed variable names

--- Lab 1/ParallaxServos-3400/ParallaxServos-3400.ino
+++ Lab 1/ParallaxServos-3400/ParallaxServos-3400.ino
@@ -1,42 +1,56 @@
 #include <Servo.h>
-  Servo rightServo;
-  Servo leftServo;
 
+// Initialize Servos
+Servo rightServo;
+Servo leftServo;
+
+// assign pins to the Servos
+const int RIGHT_PIN = 5; // Right servo control pin
+const int LEFT_PIN  = 6; // Left servo control pin
+const int LED = 7;       // External LED control pin
+
+// Inver the number for the servo to make the 0 - 90 range logic intuitive
 int invert(int number){
   return abs(number-180);
 }
-  
+
+// Turn the robot left
 void turnLeft(){
-  int valLeft = 45;
-  int valRight = 0;
+  int valLeft = 45;  // go slowly
+  int valRight = 0;  // go quickly
   leftServo.write (valLeft);
   rightServo.write(valRight);
-  delay(500);
+  delay(500);        // wait 0.5 seconds
 }
 
+// Turn the robot right
 void turnRight(){
-  int valLeft = 0;
-  int valRight = 45;
+  int valLeft = 0;    // go quickly
+  int valRight = 45;  // go slowly
   leftServo.write (valLeft);
   rightServo.write(valRight);
-  delay(500);
+  delay(500);         // wait 0.5 seconds
 }
 
+// Go straight at a certain pace
 void straight(int pace){
-  int valLeft = pace;
-  int valRight = invert(pace);
+  int valLeft = pace;          // write a pace value
+  int valRight = invert(pace); // invert the pace since the wheels opposite
   leftServo.write (valLeft);
   rightServo.write(valRight);
-  delay(1000);
+  delay(1000);                 // wait 1 second
 }
 
+// Make a (smooth) figure 8 with the robot
 void figEight(int pace){
-  straight(pace);
-  turnLeft();
+  // Make the one half of the 8
+  straight(pace);  
+  turnLeft();        
   straight(pace/2);
   turnLeft();
   straight(pace/2);
   turnLeft();
+  // Make the second half of the 8
   turnRight();
   straight(pace/2);
   turnRight();
@@ -45,19 +59,18 @@ void figEight(int pace){
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  // Attach the Servos to their corresponding pins
+  rightServo.attach(RIGHT_PIN);
+  leftServo.attach(LEFT_PIN);
 
-  rightServo.attach(5);
-  leftServo.attach(6);
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);
-  pinMode(A0, INPUT);
+  // Turn on indicator LED
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  figEight(50);
-  delay(1000);
+  figEight(50); // Draw a figure 8 at pace 50
+  delay(1000);  // Wait 1 second
 }
 
 
