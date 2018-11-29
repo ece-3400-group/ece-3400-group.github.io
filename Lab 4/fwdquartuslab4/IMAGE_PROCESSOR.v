6d882849eecb13157470e9794109f9a69f1b012b
Ta Wei Yeh <31980082+yehmostabsurd@users.noreply.github.com>
2018-11-28 21:16:14 -0500

    Edge Point detection -> Three Line detection
    The Edge Point will be violated by the noise. 
    Adding the dynamic first line when it has detected above the threshold color pixels, then the second and the third line would be based on the first line by adding 20 and 40.
    Averaging how many times had we counted the shape every 4000 frames. By doing these, we would have a more constant result. And the  LED won't keep blinking all the time.
    NOW, I am using a fix line number when detecting the color pixel numbers. It is much stable than the dynamic line detection. 
    The advantage of the dynamic line detection method is benefit when the shape is higher or lower than the center. It should be more robust when the shape isn't centered.

--- Lab 4/fwdquartuslab4/IMAGE_PROCESSOR.v
+++ Lab 4/fwdquartuslab4/IMAGE_PROCESSOR.v
@@ -30,8 +30,9 @@ assign RESULT = reg_result;
 reg [15:0] countBLUE; 
 reg [15:0] countRED; 
 reg [15:0] countNULL; 
-reg [2:0] R_THRESHOLD = 3'b100;
-reg [2:0] B_THRESHOLD = 3'b100;
+reg [8:0] Trian_CNT = 3'b0;
+reg [8:0] Sqr_CNT = 3'b0;
+reg [8:0] Diam_CNT = 3'b0;
 reg [15:0] FRAME_THRESHOLD = 16'd15000;
 reg [15:0] R_CNT_THRESHOLD = 16'd3000; 
 reg [15:0] B_CNT_THRESHOLD = 16'd3000; 	// 6000 for square (bigger area), 3000 for smaller triangle (smaller area)
@@ -42,15 +43,25 @@ reg [7:0] lastY = 8'b0;
 reg [7:0] BLUE_LINE_0 = 8'b0;
 reg [7:0] BLUE_LINE_1 = 8'b0;
 reg [7:0] BLUE_LINE_2 = 8'b0;
-reg [7:0] BLUE_CNT_LINE = 8'b0;
 // Red edge Points
 reg [7:0] RED_LINE_0 = 8'b0;
 reg [7:0] RED_LINE_1 = 8'b0;
 reg [7:0] RED_LINE_2 = 8'b0;
-reg [7:0] RED_CNT_LINE = 8'b0;
+
+// averaging enough frames for concluding the shape
+reg [12:0] frameCNT = 13'd0;
+// First Line for detection, which has enough pixels
+reg [7:0] First_redLine = 8'b0;
+reg First_redLine_found = 1'b0;
 
 always @(posedge CLK) begin 
 	if(VGA_VSYNC_NEG) begin 
+		// Count mass point of the shape for centerring the three line for shape detection, which means the height would change dynamically
+		// Mx = sum(all x) / Pixel numbers
+		// My = sum(all Y) / Pixel numbers
+		frameCNT  = frameCNT + 1'b1;
+	
+	
 		// If the camera is read as RGB323 (For more blue value) No need for green
 		// PIXEL_IN[2:0] = Blue
 		// PIXEL_IN[7:5] = Red
@@ -60,13 +71,13 @@ always @(posedge CLK) begin
 		if(PIXEL_IN[2:0] > 3'b001 && PIXEL_IN[2:0] > (PIXEL_IN[7:5] + 2'b10) && PIXEL_IN[2:0] > (PIXEL_IN[7:5] + 2'b01)  ) begin 	// && PIXEL_IN[2:0] > (PIXEL_IN[7:5] + 2'b10)
 			countBLUE = countBLUE + 16'd1; 
 			
-			if ( VGA_PIXEL_Y == 6'd42) begin
+			if ( VGA_PIXEL_Y == 52) begin
 				BLUE_LINE_0 = BLUE_LINE_0 + 1'b1;
 				end
-			if ( VGA_PIXEL_Y == 7'd72) begin
+			if ( VGA_PIXEL_Y == 72) begin
 				BLUE_LINE_1 = BLUE_LINE_1 + 1'b1;
 				end
-			if ( VGA_PIXEL_Y == 7'd102) begin
+			if ( VGA_PIXEL_Y == 102) begin
 				BLUE_LINE_2 = BLUE_LINE_2 + 1'b1;
 				end
 			/* Useless edge detecting
@@ -82,23 +93,42 @@ always @(posedge CLK) begin
 				BLUE_EdgePoint_2 = VGA_PIXEL_X;
 				end
 			*/
-				
+							
 			end 
 		else begin 
 			countNULL = countNULL + 16'd1; 
 			end
 		
 		// Usually, we have brighter red, so the red value threshold is higher.
-		if(PIXEL_IN[7:5] > 3'b100 && PIXEL_IN[7:5] > (PIXEL_IN[2:0] + 2'b10) && PIXEL_IN[7:5] > (PIXEL_IN[2:0] + 2'b01) ) begin 
+		if(PIXEL_IN[7:5] > 3'b010 && PIXEL_IN[7:5] > (PIXEL_IN[2:0] + 2'b01) && PIXEL_IN[4:3] < 2'b01 ) begin 
 			countRED = countRED + 16'd1; 
-			
-			if ( VGA_PIXEL_Y == 6'd42) begin
+			/*
+			if ( VGA_PIXEL_Y == First_redLine) begin
 				RED_LINE_0 = RED_LINE_0 + 1'b1;
+				if (RED_LINE_0 > 9'd10) begin
+					First_redLine = VGA_PIXEL_Y;
+					First_redLine_found = 1'b1;
+					end
+				else if (RED_LINE_0 < 9'd10 && First_redLine_found != 1'b0) begin
+					First_redLine = First_redLine + 1'b1;
+					RED_LINE_0 = 8'b0;
+					end
 				end
-			if ( VGA_PIXEL_Y == 7'd72) begin
+			if ( VGA_PIXEL_Y == (First_redLine + 6'd20) ) begin
 				RED_LINE_1 = RED_LINE_1 + 1'b1;
 				end
-			if ( VGA_PIXEL_Y == 7'd102) begin
+			if ( VGA_PIXEL_Y == (First_redLine + 6'd50) ) begin
+				RED_LINE_2 = RED_LINE_2 + 1'b1;
+				end
+			*/
+			
+			if ( VGA_PIXEL_Y == 8'd52 ) begin
+				RED_LINE_0 = RED_LINE_0 + 1'b1;
+				end
+			if ( VGA_PIXEL_Y == 8'd72 ) begin
+				RED_LINE_1 = RED_LINE_1 + 1'b1;
+				end
+			if ( VGA_PIXEL_Y == 8'd92 ) begin
 				RED_LINE_2 = RED_LINE_2 + 1'b1;
 				end
 			
@@ -140,21 +170,31 @@ always @(posedge CLK) begin
 //				reg_result[2] = 1'b0;
 //				end
 //		end
-		if (RED_LINE_0 > 5'd12 && RED_LINE_1 > 5'd22 && RED_LINE_2 > 5'd22 ) begin
+		if (RED_LINE_0 > 5'd10 && RED_LINE_1 > 5'd10 && RED_LINE_2 > 5'd10 ) begin
+			
 			if (RED_LINE_1 > RED_LINE_0 && RED_LINE_1 > RED_LINE_2) begin
-				reg_result[0] = 1'b1; 
-				reg_result[1] = 1'b0;
-				reg_result[2] = 1'b0;
+				// reg_result[0] = 1'b1; 
+				// reg_result[1] = 1'b0;
+				// reg_result[2] = 1'b0;
+				Diam_CNT = Diam_CNT + 1;
 				end
 			else if ( RED_LINE_2 > RED_LINE_0 && RED_LINE_2 > RED_LINE_1 ) begin
-				reg_result[0] = 1'b0; 
-				reg_result[1] = 1'b0;
-				reg_result[2] = 1'b1; 	
+				// reg_result[0] = 1'b0; 
+				// reg_result[1] = 1'b0;
+				// reg_result[2] = 1'b1; 
+				Trian_CNT = Trian_CNT + 1;
+				end
+			else if ( countRED > R_CNT_THRESHOLD ) begin
+				// ((RED_LINE_1 - RED_LINE_0) < 5'd20 || (RED_LINE_0 - RED_LINE_1) < 5'd20) || ((RED_LINE_2 - RED_LINE_1) < 5'd20 || (RED_LINE_1 - RED_LINE_2) < 5'd20) ||
+				// reg_result[0] = 1'b0; 
+				// reg_result[1] = 1'b1;
+				// reg_result[2] = 1'b0; 
+				Sqr_CNT = Sqr_CNT + 1;
 				end
 			else begin
-				reg_result[0] = 1'b0; 
-				reg_result[1] = 1'b1;
-				reg_result[2] = 1'b0;
+				// reg_result[0] = 1'b0; 
+				// reg_result[1] = 1'b0;
+				// reg_result[2] = 1'b0;
 				end
 		end
 		/*-------------------- Testing Treasure Shape -------------------------*/
@@ -170,7 +210,7 @@ always @(posedge CLK) begin
 		else if( (countBLUE <= B_CNT_THRESHOLD) && (countRED <= R_CNT_THRESHOLD) ) begin 
 			reg_result[2] = 1'b1;
 			end 
-		----------------------- Testing color ---------------------------*/
+		/*---------------------- Testing color ---------------------------*/
 	end 
 	
 	if(VGA_VSYNC_NEG == 1'b0 && lastsync == 1'b1) begin //negedge VSYNC 
@@ -184,7 +224,37 @@ always @(posedge CLK) begin
 		RED_LINE_0 = 8'b0;
 		RED_LINE_1 = 8'b0;
 		RED_LINE_2 = 8'b0;
+		First_redLine = 8'b0;
+		First_redLine_found = 1'b0;
+		frameCNT = frameCNT + 1;
 		end 
+	if ( frameCNT > 4000 ) begin
+		if (Sqr_CNT > Diam_CNT && Sqr_CNT > Trian_CNT) begin
+			reg_result[0] = 1'b0; 
+			reg_result[1] = 1'b1;
+			reg_result[2] = 1'b0;
+			end
+		else if ( Trian_CNT > Diam_CNT && Trian_CNT > Sqr_CNT) begin
+			reg_result[0] = 1'b0; 
+			reg_result[1] = 1'b0;
+			reg_result[2] = 1'b1; 	
+			end
+		else if ( Diam_CNT > Trian_CNT && Diam_CNT > Sqr_CNT) begin
+			reg_result[0] = 1'b1; 
+			reg_result[1] = 1'b0;
+			reg_result[2] = 1'b0; 	
+			end
+		else begin
+			reg_result[0] = 1'b0; 
+			reg_result[1] = 1'b0;
+			reg_result[2] = 1'b0;
+			end
+		Trian_CNT = 0;
+		Sqr_CNT = 0;
+		Diam_CNT = 0;
+		frameCNT = 0;
+	end
+	
 	lastsync = VGA_VSYNC_NEG; 
 	lastY = VGA_PIXEL_Y;
 end 
